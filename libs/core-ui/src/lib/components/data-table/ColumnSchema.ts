be267bbb847c3654746a001d18d58547f54d8dd1
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-08-08 01:52:53 +0200

    chore: extend signature for cell value and footer

--- libs/core-ui/src/lib/components/data-table/ColumnSchema.ts
+++ libs/core-ui/src/lib/components/data-table/ColumnSchema.ts
@@ -9,18 +9,19 @@ export interface CompositeCellNode {
 
 export interface ColumnSchema<T> {
   id: string;
+
   header: ReactNode;
   headerClassNames?: string | ((item: T) => string);
   /** Optional HTML attributes for the header `<th>` element. */
   headerCellProps?: ThHTMLAttributes<HTMLTableHeaderCellElement>;
 
-  value: (item: T) => CompositeCellNode | ReactNode;
+  value: ReactNode | ((item: T) => CompositeCellNode | ReactNode);
   cellTitle?: (item: T) => string | undefined;
   cellClassNames?: string | ((item: T, cellValue: CellValue) => string);
   /** Optional HTML attributes for the body `<td>` element. */
   cellProps?: TdHTMLAttributes<HTMLTableDataCellElement>;
 
-  footer?: (data: T[]) => CompositeCellNode | ReactNode;
+  footer?: ReactNode | ((data: T[]) => CompositeCellNode | ReactNode);
   footerClassNames?: string | ((data: T[], cellValue: CellValue) => string);
   /** Optional HTML attributes for the footer `<td>` element, e.g., for `colSpan`. */
   footerCellProps?: TdHTMLAttributes<HTMLTableDataCellElement>;
