8987e60572ef1b8283024d61fd5814ce221aa771
Marco Wegner <673439+marvey11@users.noreply.github.com>
2025-08-15 23:34:55 +0200

    refactor: fix potential race condition in portfolio details page

--- apps/portfolio-frontend/src/types/misc-types.ts
+++ apps/portfolio-frontend/src/types/misc-types.ts
@@ -4,3 +4,10 @@ export interface QuoteObject {
 }
 
 export type LatestQuoteMapping = Map<string, QuoteObject>;
+
+export interface XIRRObject {
+  date: Date;
+  xirr: number;
+}
+
+export type XIRRMapping = Map<string, XIRRObject>;
