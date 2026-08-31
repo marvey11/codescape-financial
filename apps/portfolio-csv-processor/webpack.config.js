96d675a407485f3100b2e5213ae4dc0c0cec3f9b
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-31 20:52:35 +0000

    chore: fix webpack configs

--- apps/portfolio-csv-processor/webpack.config.js
+++ apps/portfolio-csv-processor/webpack.config.js
@@ -1,20 +1,6 @@
-const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
 const { join } = require("path");
+const { createNodeWebpackConfig } = require(
+  join(process.cwd(), "webpack.base.config"),
+);
 
-module.exports = {
-  output: {
-    path: join(__dirname, "dist"),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: "node",
-      compiler: "tsc",
-      main: "./src/main.ts",
-      tsConfig: "./tsconfig.app.json",
-      assets: ["./src/assets"],
-      optimization: false,
-      outputHashing: "none",
-      generatePackageJson: true,
-    }),
-  ],
-};
+module.exports = createNodeWebpackConfig(__dirname);
