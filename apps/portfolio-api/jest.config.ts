d5d97a9332e8e67c99908ca5696574ce8708eeac
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-31 15:41:01 +0000

    chore: upgrade nx to v22

--- apps/portfolio-api/jest.config.ts
+++ apps/portfolio-api/jest.config.ts
@@ -1,21 +1,21 @@
 /* eslint-disable */
-import { readFileSync } from 'fs';
+const { readFileSync } = require("fs");
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+  readFileSync(`${__dirname}/.spec.swcrc`, "utf-8"),
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false;
 
-export default {
-  displayName: '@codescape-financial/portfolio-api',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
+module.exports = {
+  displayName: "@codescape-financial/portfolio-api",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+    "^.+\\.[tj]s$": ["@swc/jest", swcJestConfig],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'test-output/jest/coverage',
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "test-output/jest/coverage",
 };
