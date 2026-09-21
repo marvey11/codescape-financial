b5c3c082936d08ea00bcb3db3ab273708ca04402
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-09-21 10:24:25 +0000

    chore: fix linting configurations

--- libs/core-ui/eslint.config.mjs
+++ libs/core-ui/eslint.config.mjs
@@ -1,12 +1,9 @@
-import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
+import baseConfig from "../../eslint.config.mjs";
 
 export default [
   ...baseConfig,
-  ...nx.configs['flat/react'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
     rules: {},
   },
 ];
